04761522dd7d27f9043f7f0f3a514a1123a3ac0a
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-05-28 03:58:00 +0900

    バットを配布するコマンドを追加

--- Baseball/src/rain1208/baseball/Main.php
+++ Baseball/src/rain1208/baseball/Main.php
@@ -3,6 +3,10 @@
 
 namespace rain1208\baseball;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\Stick;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
@@ -12,4 +16,17 @@ class Main extends PluginBase
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(),$this);
     }
 
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        switch ($label) {
+            case "bat":
+                if ($sender instanceof Player) {
+                    $item = new Stick(1);
+                    $item->setCustomName("バット");
+                    $sender->getInventory()->setItemInHand($item);
+                }
+                break;
+        }
+        return true;
+    }
 }
