ca39175ee75cca429a2a28dc64f6828270e4f78e
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-06-12 14:13:48 +0900

    ゲームスタートをコマンドからformに変更

---
+++ oniGame/src/rain1208/onigame/game/form/StartGameForm.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace rain1208\onigame\game\form;
+
+
+use pocketmine\form\Form;
+use pocketmine\form\FormValidationException;
+use pocketmine\Player;
+
+class StartGameForm implements Form
+{
+    public function handleResponse(Player $player, $data): void
+    {
+        // TODO: Implement handleResponse() method.
+    }
+
+    public function jsonSerialize()
+    {
+        // TODO: Implement jsonSerialize() method.
+    }
+}
