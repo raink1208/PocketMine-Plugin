6a4116ab2922b9dd56d4e3284dd22cda4eb3842c
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-06-12 14:12:09 +0900

    鬼の交代

--- oniGame/src/rain1208/onigame/event/GameEventListener.php
+++ oniGame/src/rain1208/onigame/event/GameEventListener.php
@@ -3,9 +3,11 @@
 
 namespace rain1208\onigame\event;
 
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerCreationEvent;
 use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\Server;
 use rain1208\onigame\GamePlayer;
 
 class GameEventListener implements Listener
@@ -14,7 +16,19 @@ class GameEventListener implements Listener
     {
         $event->setPlayerClass(GamePlayer::class);
     }
+
     public function onJoin(PlayerLoginEvent $event) {
         $event->getPlayer()->initPlayer();
+    }
+
+    public function changeOni(EntityDamageByEntityEvent $event)
+    {
+        $damager = $event->getDamager();
+        $hitter = $event->getEntity();
+        if ($damager instanceof GamePlayer && $hitter instanceof GamePlayer) {
+            if ($damager->isOni() && $hitter->isPlaying()) {
+                Server::getInstance()->broadcastMessage("鬼が交代します");
+            }
+        }
     }
 }
